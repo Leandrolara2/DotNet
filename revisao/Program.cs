92ab9f28b160f3b3d25a3fbe2f8fecba48e91645
LaraLeandro2 <laraleandro2@gmail.com>
2021-08-09 16:32:22 -0300

    Aprendendo DotNet parte 2

--- revisao/Program.cs
+++ revisao/Program.cs
@@ -4,48 +4,61 @@ namespace revisao
 {
     class Program
     {
+        Aluno[] alunos = new Aluno[5];
         static void Main(string[] args)
         {
-            Console.WriteLine("Indorme a opção desejada");
-            Console.WriteLine("1- Inserir novo aluno");
-            Console.WriteLine("2- Listar alunos");    
-            Console.WriteLine("3- calcular media geral");
-            Console.WriteLine("X- Sair");
-            Console.WriteLine();
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-           string opcaoUsuario = Console.ReadLine();
-
-           while (opcaoUsuario.ToUpper() !="X")
-           {
-               switch(opcaoUsuario)
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
                 {
                     case "1":
                         //TODO: adicionar aluno
+                        Console.WriteLine("Informe o nome do Aluno: ");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota do aluno: ");
+                        
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nome = nota;
+                        }
+
                         break;
 
                     case "2":
                         //TODO: Listar alunos
                         break;
-                    
+
                     case "3":
                         //TODO: Calcular media geral
                         break;
 
                     default:
-                    throw new ArgumentOutOfRangeException();
+                        throw new ArgumentOutOfRangeException();
 
                 }
-                Console.WriteLine("Indorme a opção desejada");
-                Console.WriteLine("1- Inserir novo aluno");
-                Console.WriteLine("2- Listar alunos");    
-                Console.WriteLine("3- calcular media geral");
-                Console.WriteLine("X- Sair");
-                Console.WriteLine();
 
-                opcaoUsuario = Console.ReadLine();
+                opcaoUsuario = ObterOpcaoUsuario();
 
-           }
-            
+            }
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Indorme a opção desejada");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- calcular media geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+             return opcaoUsuario;
         }
     }
 }
