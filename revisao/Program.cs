884a3f8630e9ae2b5fc8b84fcb1936350600c649
LaraLeandro2 <laraleandro2@gmail.com>
2021-08-09 13:16:38 -0300

    Aprendendo DotNet

---
+++ revisao/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indorme a opção desejada");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");    
+            Console.WriteLine("3- calcular media geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+           string opcaoUsuario = Console.ReadLine();
+
+           while (opcaoUsuario.ToUpper() !="X")
+           {
+               switch(opcaoUsuario)
+                {
+                    case "1":
+                        //TODO: adicionar aluno
+                        break;
+
+                    case "2":
+                        //TODO: Listar alunos
+                        break;
+                    
+                    case "3":
+                        //TODO: Calcular media geral
+                        break;
+
+                    default:
+                    throw new ArgumentOutOfRangeException();
+
+                }
+                Console.WriteLine("Indorme a opção desejada");
+                Console.WriteLine("1- Inserir novo aluno");
+                Console.WriteLine("2- Listar alunos");    
+                Console.WriteLine("3- calcular media geral");
+                Console.WriteLine("X- Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();
+
+           }
+            
+        }
+    }
+}
